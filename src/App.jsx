851ecb31c30b3deeb3ead13539ac2a28bb068ae8
user <user>
2020-01-27 10:01:33 +0000

    The data for the LetterCard is now provided by it's parent.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
 import 'typeface-roboto';
 import MenuIcon from "@material-ui/icons/Menu";
 import LetterCard from './components/LetterCard/LetterCard';
 
-function App() {
-  return (
-      <div >
-	<AppBar position="static">
-	  <Toolbar>
-	    <IconButton edge="start"  color="inherit" aria-label="menu">
-	      <MenuIcon />
-	    </IconButton>
-	    <Typography variant="h6" >
-	      B1 Briefe Online
-	    </Typography>
-	  </Toolbar>
-	</AppBar>
-	<LetterCard />
-      </div>
-  );
+class App extends Component {
+
+    constructor() {
+	super();
+	this.state = {
+	    expanded: false,
+	    demoData: {
+		title: "Heizungsproblem",
+		problem: `In Ihrer Wohnung haben Sie seit einiger Zeit Probleme mit der Heizung. Der Vermieter soll die Heizung reparieren lassen. Leider k&ouml;nnen Sie Ihren Vermieter telefonisch nicht erreichen, deshalb schreiben Sie einen Brief.</p>
+<p>Grund des Schreibens:</p>
+<p>a - Problem: wie lange schon?<br/>b - Termin f&uuml;r Reparatur<br/>c - wie Sie erreichbar sind</p>`,
+		solution: `<p><br/>Sehr geehrter Herr M&ouml;ller,</p>
+<p>ich schreibe Ihnen, weil ich in meiner Wohnung ein Problem mit der Heizung habe. Ich habe Sie angerufen, aber leider konnte ich Sie nicht erreichen. Es ist sehr dringend, denn seit drei Tagen ist die Heizung kaputt. Seit gestern ist es drau&szlig;en kalt und in der Wohnung sind nur 16 Grad. Das ist zu kalt f&uuml;r mich und meine Familie - meine Tochter hat schon eine Erk&auml;ltung!<br/>Wann k&ouml;nnen Sie die Heizung reparieren oder einen Handwerker schicken? Mein Mann (meine Frau) ist jeden Nachmittag zu Hause, Sie k&ouml;nnen einfach anrufen und kommen. Oder Sie rufen mich an, meine Handynummer ist 0 1520 278494 ...</p>
+<p>Mit freundlichen Gr&uuml;&szlig;en</p>
+<p>(Ihr Vor- und Familienname)</p>`
+	    }
+	};
+    }
+
+    render() {
+	return (
+	    <div >
+	      <AppBar position="static">
+		<Toolbar>
+		  <IconButton edge="start"  color="inherit" aria-label="menu">
+		    <MenuIcon />
+		  </IconButton>
+		  <Typography variant="h6" >
+		    B1 Briefe Online
+		  </Typography>
+		</Toolbar>
+	      </AppBar>
+	      <LetterCard letterData={ this.state.demoData } />
+	    </div>
+	);
+    }
 }
 
 export default App;
--- src/components/LetterCard/LetterCard.jsx
+++ src/components/LetterCard/LetterCard.jsx
@@ -11,16 +11,6 @@ export default class LetterCard extends Component {
 	super(props);
 	this.state = {
 	    expanded: false,
-	    demoData: {
-		title: "Heizungsproblem",
-		problem: `In Ihrer Wohnung haben Sie seit einiger Zeit Probleme mit der Heizung. Der Vermieter soll die Heizung reparieren lassen. Leider k&ouml;nnen Sie Ihren Vermieter telefonisch nicht erreichen, deshalb schreiben Sie einen Brief.</p>
-<p>Grund des Schreibens:</p>
-<p>a - Problem: wie lange schon?<br/>b - Termin f&uuml;r Reparatur<br/>c - wie Sie erreichbar sind</p>`,
-		solution: `<p><br/>Sehr geehrter Herr M&ouml;ller,</p>
-<p>ich schreibe Ihnen, weil ich in meiner Wohnung ein Problem mit der Heizung habe. Ich habe Sie angerufen, aber leider konnte ich Sie nicht erreichen. Es ist sehr dringend, denn seit drei Tagen ist die Heizung kaputt. Seit gestern ist es drau&szlig;en kalt und in der Wohnung sind nur 16 Grad. Das ist zu kalt f&uuml;r mich und meine Familie - meine Tochter hat schon eine Erk&auml;ltung!<br/>Wann k&ouml;nnen Sie die Heizung reparieren oder einen Handwerker schicken? Mein Mann (meine Frau) ist jeden Nachmittag zu Hause, Sie k&ouml;nnen einfach anrufen und kommen. Oder Sie rufen mich an, meine Handynummer ist 0 1520 278494 ...</p>
-<p>Mit freundlichen Gr&uuml;&szlig;en</p>
-<p>(Ihr Vor- und Familienname)</p>`
-	    }
 	};
 	
     }
@@ -35,9 +25,9 @@ export default class LetterCard extends Component {
     render() {
 	return (
 	    <Card>
-	      <CardHeader title={this.state.demoData.title}/>
+	      <CardHeader title={this.props.letterData.title}/>
 	      <CardContent>
-		<Typography dangerouslySetInnerHTML={this.createMarkup(this.state.demoData.problem)}>
+		<Typography dangerouslySetInnerHTML={this.createMarkup(this.props.letterData.problem)}>
 		</Typography>
 	      </CardContent>
 	      <CardActions>
@@ -47,7 +37,7 @@ export default class LetterCard extends Component {
 	      </CardActions>
 	      <Collapse in={this.state.expanded}>
 		<CardContent>
-		  <Typography dangerouslySetInnerHTML={this.createMarkup(this.state.demoData.solution)}>
+		  <Typography dangerouslySetInnerHTML={this.createMarkup(this.props.letterData.solution)}>
 		  </Typography>
 		</CardContent>
 	      </Collapse>
